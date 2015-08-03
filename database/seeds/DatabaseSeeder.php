557bca7486ff1f4187eff93eb4d9f6a20222875b
Xavier Au <xavier.au@anacreation.com>
2015-08-03 18:22:12 +0800

    update migration file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Support\Facades\DB;
     use Xavierau\RoleBaseAuthentication\database\Initializer;
     use Xavierau\RoleBaseAuthentication\database\seeds\PermissionTableSeeder;
     use Xavierau\RoleBaseAuthentication\database\seeds\RoleTableSeeder;
@@ -17,10 +18,19 @@ use Illuminate\Database\Eloquent\Model;
     {
         Model::unguard();
 
-         $this->call(UserTableSeeder::class);
+        //disable foreign key check for this connection before running seeders
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        $this->call(UserTableSeeder::class);
         $this->call(LanguageTableSeeder::class);
         $this->call(PermissionTableSeeder::class);
         $this->call(RoleTableSeeder::class);
+
+        (new Initializer())->initialize();
+        
+        // supposed to only apply to a single connection and reset it's self
+        // but I like to explicitly undo what I've done for clarity
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         Model::reguard();
     }
